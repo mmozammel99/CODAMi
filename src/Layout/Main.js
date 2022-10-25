ac0b9bca9eb8bfe2bd8645c764a5e48f7b540843
mmozammel99 <mhfaryanemon@gmail.com>
2022-10-25 20:58:58 +0600

    header add

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Page/SharePage/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '../Page/SharePage/Footer';
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
 
         </div>
     );
